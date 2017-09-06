91efbbbc02d00e5ace8c5232dba9f2305ad6fd91
NxDon <645104928@qq.com>
2017-09-06 16:24:17 +0800

    extract a SearchInput component

--- app/components/HomeHeader/index.js
+++ app/components/HomeHeader/index.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
-import {Link} from 'react-router'
-import './index.less'
+import {Link,hashHistory} from 'react-router'
 
+import './index.less'
+import SearchInput from '../SearchInput'
 class HomeHeader extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
     }
+
+    enterHandle(value) {
+        hashHistory.push('/search/all/' + encodeURIComponent(value))
+    }
+
     render() {
         return (
             <div id="home-header" className="clear-fix">
@@ -22,7 +28,7 @@ class HomeHeader extends React.Component {
                 <div className="home-header-middle">
                     <div className="search-container">
                         <i className="icon-search"></i>
-                        <input type="text" placeholder="请输入关键字"/>
+                        <SearchInput value="" enterHandle={this.enterHandle.bind(this)}/>
                     </div>
                 </div>
             </div>
